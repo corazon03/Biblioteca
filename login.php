1883a3549a5b45efe7d6ca8502a2c6250a0536a1
corazon03 <82195740+corazon03@users.noreply.github.com>
2022-06-11 13:40:48 -0600

    Inicio Sesión sin footer

--- login.php
+++ login.php
@@ -5,20 +5,29 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Biblioteca</title>
-    <link href="estilos.css" rel="stylesheet" type="text/css">
+    <link href="estilo.css" rel="stylesheet" type="text/css">
 </head>
 <body>
     <div id="logo">
         <h1>Biblioteca</h1>
     </div>
     <div id="inicioSesion">
-        <form action="index.php" method="post">
-            <label>Usuario</label><br/>
-            <input type="text" placeholder="Usuario..."><br/>
-            <label>Contraseña</label><br/>
-            <input type="text" placeholder="Contraseña..."><br/>
-            <input type="submit" value="Iniciar sesión">
-        </form>
+        <div id="img">
+            <img src="img/Libro.png">
+        </div>
+        <div id="form">
+            <form action="index.php" method="post">
+                <label>Usuario</label><br/>
+                <input type="text" placeholder="Usuario..."><br/>
+                <label>Contraseña</label><br/>
+                <input type="text" placeholder="Contraseña..."><br/>
+                <input type="submit" value="Iniciar sesión">
+                <div id="registro">
+                    <p>¿Aún no tienes cuenta?</p>
+                    <a href="registro.php"><button>Regístrate</button></a>
+                </div>
+            </form>
+        </div>
     </div>
 </body>
 </html>
